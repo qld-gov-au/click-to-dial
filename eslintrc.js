55ca83689b03b8ddfb16abe69e41cdb2a314ba5f
KarlHearne <129135955+KarlHearne@users.noreply.github.com>
2025-02-11 15:21:12 +1000

    Update eslintrc.js

--- eslintrc.js
+++ eslintrc.js
@@ -1,17 +1,18 @@
-module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+mimport globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+
+export default  [
+  { files: ["**/*.js"], languageOptions: {sourceType: "commonjs"} },
+  { languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { languageOptions: { parserOptions: { ecmaVersion: "latest"} } },
+  pluginJs.configs.recommended,
+  { rules: { "eqeqeq" : "error", "max-len": ["error", 80] }}
+]
+
+
+
+
+
